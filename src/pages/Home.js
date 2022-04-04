f79de7b0039deaf17d811259f14112b6363c7c1c
DH7789-dev <dharnaud77@hotmail.fr>
2022-04-04 18:48:25 +0200

    fix excute code with api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,33 @@
+import CodeMirror from "@uiw/react-codemirror"
+import "codemirror/theme/material-darker.css"
+import {useEffect, useState} from "react"
+import axios from "axios";
 
 const Home = () => {
+    const [codeTest, setCodeTest] =  useState(``)
+    const getCodeTest = (codetitle) => {
+        const article = { code: codetitle };
+        axios.post('http://localhost:3001/excution/python', article)
+            .then(response => console.log(response.data.response));
+    }
+
 
     return(
         <div className='view--home'>
             <div>HOME</div>
+            <div>
+                <CodeMirror
+                    options={{theme : "material-darker", mode: "python"}}
+                    value=""
+                    height="400px"
+                    onChange={(editor, viewUpdate) => {
+                        console.log('value:', editor.getValue());
+                        setCodeTest(editor.getValue())
+                    }}/>
+            </div>
+
+            <button style={{backgroundColor: "#FFFBED"}} onClick={() => getCodeTest(codeTest)}> excute </button>
+
         </div>
     )
 }
