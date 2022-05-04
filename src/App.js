3f08a19d946452062e92d8fb4eb2feb9a5e9a0fd
DH7789-dev <dharnaud77@hotmail.fr>
2022-05-04 11:24:34 +0200

    [fix] App and Assets

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 
 import Home from "./pages/Home";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
+import MainHeader from "./layout/Main-Header";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Project from "./pages/Project";
+import NotFound from "./components/error/NotFound";
+import Code from "./pages/Code";
 
 
 const App =  () => {
   return (
       <Router>
-        <Switch>
-          <Route path="/" exact component={Home} />
-        </Switch>
+        <MainHeader/>
+          <Switch>
+              <Route path="/login" exact component={Login} />
+              <Route path="/register" exact component={Register} />
+              <Route path="/project" exact component={Project} />
+              <Route path="/code/:id" exact component={Code} />
+              <Route path="/" exact component={Home} />
+              <Route to="/404" component={NotFound} />
+              <Redirect to="/404" />
+          </Switch>
       </Router>
 
   );
