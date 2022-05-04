32987a4b70a19e51622d2900f9cc528c4823570f
DH7789-dev <dharnaud77@hotmail.fr>
2022-05-04 11:23:59 +0200

    [fix] auth

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const SignIn = () => {
+const Register = () => {
     return (
         <div className="view--auth">
             <div className="right-part-auth">
                 <img className="right-img-auth" src="/assets/img/signUp.svg" alt="register"/>
             </div>
             <div className="left-part-auth">
-                <div className="title-auth-left">register</div>
+                <div className="title-auth-left">Register</div>
                 <form action="" className="auth-from">
                     <input type="text" placeholder="Lastname" className="form-input"/>
                     <input type="text" placeholder="firstname" className="form-input"/>
@@ -16,10 +16,11 @@ const SignIn = () => {
                     <input type="text" placeholder="email" className="form-input"/>
                     <input type="text" placeholder="tel" className="form-input"/>
                 </form>
+                <div className="button-send">register</div>
             </div>
-            <div className="button-send">Login</div>
+
         </div>
     );
 };
 
-export default SignIn;
+export default Register;
