91693e5c6504e6f30e39dba9fd1e08647490b0bb
DH7789-dev <dharnaud77@hotmail.fr>
2022-05-09 18:37:34 +0200

    [feature] profil

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,45 @@
+
+B
+src/pages/Login.js,c/8/c896236ba4a30f2d4d14daebfc43b8b237ff6821
+E
+src/pages/Register.js,6/b/6b70fd87d4ba5ad5f02598f6e9ca4a2c38eb0fb1
+P
+ src/sass/layout/main-header.scss,6/0/60c1361b552acdc778c0aff8957a73d7bb2f83e5
+J
+src/sass/utils/_reset.scss,a/6/a6a67ee8c4c7751e697180cc01cd251d18e568d3
+H
+src/sass/layout/nav.scss,d/8/d83de6d456940ff2d0931607460a17f21bcf9c4f
+W
+'src/components/error/particles-404.json,d/1/d1be5794a790982937caa4dd8e1d249a3986d149
+P
+ src/components/pages/CardHome.js,c/a/ca691b250f2c4e1b4a97bdee1e3481cbfb4b41ab
+R
+"src/sass/pages/error/notfound.scss,4/5/45233e7af93b39942480f1e5ba13cc750eb25020
+P
+ src/components/error/NotFound.js,6/1/616f36efc17ca98f8dd1a85989ceb0d1786f5626
+X
+(src/sass/components/pages/card-home.scss,b/5/b598b8a96893a6071dce9d0026ca66686ff94c73
+H
+src/sass/pages/home.scss,8/0/8073511c35d5ad5e4260404e95c8e1689e759f30
+M
+src/sass/utils/_settings.scss,e/d/ede314402a11606ed02f8d9f915d97148b3e6b44
+V
+&src/components/pages/CardSearchCode.js,7/3/7358b16b25b3bed451b416e6a72c464ce8e4417a
+H
+src/sass/pages/code.scss,c/c/cc4e6c7e4bccddef2f3383e5b3651df3f51a5436
+]
+-src/sass/components/pages/cardsearchcode.scss,3/9/394dbba44f7ea44b3d6bb04da45cfc8efe085042
+D
+src/pages/Project.js,c/4/c414b9c6a8b35d3687649f04e03a82e323aaefec
+A
+src/pages/Code.js,0/a/0a07a77dc29df517cc8b79c5e6bc8c612df0336f
+K
+src/sass/pages/project.scss,6/f/6f63b46266c2764ca52575b8ffaa16c698bd8e78
+H
+src/sass/pages/auth.scss,7/a/7acd5e313c40e8c05b40a776303083e8a708cec1
+A
+public/index.html,f/4/f43ecdf8d666ee908aacce037b4cd72159e4741b
+\
+,src/sass/components/layout/modal-header.scss,e/b/eb729cee071e17a6f80b3ce88d7686a8aff42b13
+A
+src/sass/App.scss,6/0/604945b3cff347060e33059d1c80d0011222fcb6
--- src/pages/Profil.js
+++ src/pages/Profil.js
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Profil = () => {
+
+    const [firstName,setFirstName] = useState("David")
+    const [lastName,setLastName] = useState("Arnaud")
+    const [pseudo,setPseudo] = useState("DH7789-dev")
+    const [mdp,setMdp] = useState("dada222329@")
+    const [tel,setTel] = useState("0622901123")
+    const [mail,setMail] = useState("David@gmail.com")
+
     return (
-        <div>
-            
+        <div className="view--profile">
+            <div className="header-profile">
+                <img className="overlay-profile" src="/assets/img/overlay-profile.svg" alt=""/>
+                <div className="profile-data">
+                    <div className="title-name">{pseudo}</div>
+                    <div className="image-profile">
+                        <img src="/assets/logo/profil_header.svg" alt="profile"/>
+                    </div>
+                </div>
+            </div>
+            <div className="body-profile">
+                <div className="social-profile">
+                    <div className="social-follow">
+                        <div className="number-social-follow">0</div>
+                        <div className="title-social-follow">followers</div>
+                    </div>
+                    <div className="social-follow">
+                        <div className="number-social-follow">0</div>
+                        <div className="title-social-follow">following</div>
+                    </div>
+                </div>
+                <form action="" className="form-profile">
+                    <div className="container-profile">
+                        <div className="social-profile-input">
+                            <div className="title-input">Nom</div>
+                            <input type="text" className="input-profile" defaultValue={lastName}/>
+                        </div>
+                        <div className="social-profile-input">
+                            <div className="title-input">Prenom</div>
+                            <input type="text" className="input-profile" defaultValue={firstName}/>
+                        </div>
+                    </div>
+                    <div className="container-profile">
+                        <div className="social-profile-input">
+                            <div className="title-input">Pseudo</div>
+                            <input type="text" className="input-profile" defaultValue={pseudo}/>
+                        </div>
+                        <div className="social-profile-input">
+                            <div className="title-input">MDP</div>
+                            <input type="password" className="input-profile" defaultValue={mdp}/>
+                        </div>
+                    </div>
+                    <div className="container-profile">
+                        <div className="social-profile-input">
+                            <div className="title-input">Tel</div>
+                            <input type="tel" className="input-profile" defaultValue={tel}/>
+                        </div>
+                        <div className="social-profile-input">
+                            <div className="title-input">Email</div>
+                            <input type="text" className="input-profile" defaultValue={mail}/>
+                        </div>
+                    </div>
+                    <button className="button-profile">enregistrer</button>
+                </form>
+            </div>
         </div>
     );
 };
