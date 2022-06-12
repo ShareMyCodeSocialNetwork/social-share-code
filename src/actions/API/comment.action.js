205c11b65c46ee50da87fb4d7dd5a9f06af60ce0
xof <ljehanno@myges.fr>
2022-06-13 00:06:44 +0200

    :sparkles: add missing api routes

--- src/actions/API/comment.action.js
+++ src/actions/API/comment.action.js
@@ -6,6 +6,8 @@ import AuthService from "../../components/Auth/AuthService";
 export const API_URL = "https://localhost:8080"
 export const GET_COMMENT = "GET_COMMENT";
 export const GET_COMMENT_BY_ID = "GET_COMMENT_BY_ID";
+export const GET_COMMENTS_BY_USER = "GET_COMMENTS_BY_USER";
+export const GET_COMMENTS_BY_POST = "GET_COMMENTS_BY_POST";
 export const ADD_COMMENT = "ADD_COMMENT";
 export const UPDATE_COMMENT = "UPDATE_COMMENT";
 export const DELETE_COMMENT = "DELETE_COMMENT";
@@ -52,7 +54,7 @@ export const addComment = (data) => {
 export const updateComment = (commentId,data) => {
     return (dispatch) => {
         return axios
-            .post(`${API_URL}/comment/update/${commentId}`, data, { headers:  AuthService.authHeader() })
+            .patch(`${API_URL}/comment/update/${commentId}`, data, { headers:  AuthService.authHeader() })
             .then(() => {
                 dispatch({ type: UPDATE_COMMENT, payload: data });
             })
@@ -72,6 +74,28 @@ export const deleteComment = (commentId) => {
         })
             .then(() => {
                 dispatch({ type: DELETE_COMMENT, payload: { commentId } });
+            })
+            .catch((err) => console.log(err));
+    };
+};
+
+export const getCommentsByUser = (userId) => {
+    return (dispatch) => {
+        return axios
+            .get(`${API_URL}/comment/user/${userId}`,{ headers:  AuthService.authHeader() })
+            .then((res) => {
+                dispatch({ type: GET_COMMENTS_BY_USER, payload: res.data });
+            })
+            .catch((err) => console.log(err));
+    };
+};
+
+export const getCommentsByPost = (postId) => {
+    return (dispatch) => {
+        return axios
+            .get(`${API_URL}/comment/post/${postId}`,{ headers:  AuthService.authHeader() })
+            .then((res) => {
+                dispatch({ type: GET_COMMENTS_BY_POST, payload: res.data });
             })
             .catch((err) => console.log(err));
     };
