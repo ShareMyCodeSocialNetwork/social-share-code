4c09d9bf042bc167f2e6f903d30738aa02b53e87
xof <ljehanno@myges.fr>
2022-06-13 15:28:30 +0200

    :sparkles: add missing reducer like

--- src/reducers/API/like.reducer.js
+++ src/reducers/API/like.reducer.js
@@ -1,30 +1,23 @@
 import {
-    GET_LIKE,
+    GET_LIKES,
     GET_LIKE_BY_ID,
     ADD_LIKE,
-    UPDATE_LIKE,
-    DELETE_LIKE
+    DELETE_LIKE,
+    GET_POST_LIKE
 } from "../../actions/API/like.action";
 
 const initialState = {};
 
 export default function likeReducer(state = initialState, action) {
     switch (action.type) {
-        case GET_LIKE:
+        case GET_LIKES:
+            return action.payload;
+        case GET_POST_LIKE:
             return action.payload;
         case GET_LIKE_BY_ID:
             return action.payload;
         case ADD_LIKE:
             return [action.payload, ...state];
-        case UPDATE_LIKE:
-            return state.map((team) => {
-                if (team.id === action.payload.id) {
-                    return {
-                        ...team,
-                        content: action.payload.content,
-                    };
-                } else return team;
-            });
         case DELETE_LIKE:
             return state.filter((LIKE) => LIKE.id !== action.payload.likeId);
         default:
