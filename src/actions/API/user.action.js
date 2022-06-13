02ccb0f648b78ffb11f9f262ff51e7908eaff90d
xof <ljehanno@myges.fr>
2022-06-13 14:08:16 +0200

    :sparkles: add missing api routes user

--- src/actions/API/user.action.js
+++ src/actions/API/user.action.js
@@ -9,6 +9,7 @@ export const GET_USER = "GET_USER";
 export const GET_USER_BY_ID = "GET_USER_BY_ID";
 export const GET_USER_BY_USER_ID = "GET_USER_BY_USER_ID";
 export const GET_USER_BY_PSEUDO = "GET_USER_BY_PSEUDO";
+export const GET_USER_BY_EMAIL = "GET_USER_BY_EMAIL";
 export const ADD_USER = "ADD_USER";
 export const UPDATE_USER_EMAIL = "UPDATE_USER_EMAIL";
 export const UPDATE_USER_PSEUDO = "UPDATE_USER_PSEUDO";
@@ -53,6 +54,17 @@ export const getOneUserByPseudo = (pseudo) => {
     };
 };
 
+export const getUserByEmail = (email) => {
+    return (dispatch) => {
+        return axios
+            .get(`${API_URL}/user/email/${email}`,{ headers:  AuthService.authHeader() })
+            .then((res) => {
+                dispatch({ type: GET_USER_BY_EMAIL, payload: res.data });
+            })
+            .catch((err) => console.log(err));
+    };
+};
+
 export const getOneUserByUserId = (userId) => {
     return (dispatch) => {
         return axios
@@ -69,7 +81,7 @@ export const getOneUserByUserId = (userId) => {
 export const updateUserPassword = (userId,data) => {
     return (dispatch) => {
         return axios
-            .post(`${API_URL}/user/update/password/${userId}`, data, { headers:  AuthService.authHeader() })
+            .patch(`${API_URL}/user/update/password/${userId}`, data, { headers:  AuthService.authHeader() })
             .then(() => {
                 dispatch({ type: UPDATE_USER_PASSWORD, payload: data });
             })
@@ -80,7 +92,7 @@ export const updateUserPassword = (userId,data) => {
 export const updateUserFirstName = (userId,data) => {
     return (dispatch) => {
         return axios
-            .post(`${API_URL}/user/update/firstname/${userId}`, data, { headers:  AuthService.authHeader() })
+            .patch(`${API_URL}/user/update/firstname/${userId}`, data, { headers:  AuthService.authHeader() })
             .then(() => {
                 dispatch({ type: UPDATE_USER_FIRST_NAME, payload: data });
             })
@@ -91,7 +103,7 @@ export const updateUserFirstName = (userId,data) => {
 export const updateUserLastName = (userId,data) => {
     return (dispatch) => {
         return axios
-            .post(`${API_URL}/user/update/lastname/${userId}`, data, { headers:  AuthService.authHeader() })
+            .patch(`${API_URL}/user/update/lastname/${userId}`, data, { headers:  AuthService.authHeader() })
             .then(() => {
                 dispatch({ type: UPDATE_USER_LAST_NAME, payload: data });
             })
@@ -103,7 +115,7 @@ export const updateUserLastName = (userId,data) => {
 export const updateUserEmail = (userId,data) => {
     return (dispatch) => {
         return axios
-            .post(`${API_URL}/user/update/email/${userId}`, data, { headers:  AuthService.authHeader() })
+            .patch(`${API_URL}/user/update/email/${userId}`, data, { headers:  AuthService.authHeader() })
             .then(() => {
                 dispatch({ type: UPDATE_USER_EMAIL, payload: data });
             })
@@ -115,7 +127,7 @@ export const updateUserEmail = (userId,data) => {
 export const updateUserPseudo = (userId,data) => {
     return (dispatch) => {
         return axios
-            .post(`${API_URL}/user/update/pseudo/${userId}`, data, { headers:  AuthService.authHeader() })
+            .patch(`${API_URL}/user/update/pseudo/${userId}`, data, { headers:  AuthService.authHeader() })
             .then(() => {
                 dispatch({ type: UPDATE_USER_PSEUDO, payload: data });
             })
