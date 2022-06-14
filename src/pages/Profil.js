108036683daa75b8d03ffa9161798f15d68fd44f
xof <ljehanno@myges.fr>
2022-06-14 22:27:47 +0200

    :rocket: get current user info (need to be diisplayed)

--- src/pages/Profil.js
+++ src/pages/Profil.js
@@ -1,54 +1,129 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useForm} from "react-hook-form";
 import AuthService from "../components/Auth/AuthService";
-import {Redirect, useHistory} from "react-router-dom";
-import { updateUserEmail, updateUserFirstName, updateUserLastName, updateUserPassword, updateUserPseudo } from "../actions/API/user.action";
-import {useDispatch} from "react-redux";
-import {isEmpty, wait} from "../components/utils/Utils";
+import {useHistory} from "react-router-dom";
+import {
+    getUserByEmail,
+    updateUserEmail,
+    updateUserFirstName,
+    updateUserLastName,
+    updateUserPassword,
+    updateUserPseudo
+} from "../actions/API/user.action";
+import {useDispatch, useSelector} from "react-redux";
+import {wait} from "../components/utils/Utils";
+//import {getFollowed, getFollowers} from "../actions/API/follower.action";
 
 const Profil = () => {
-    const history = useHistory();
     const dispatch = useDispatch();
-    /*if(AuthService.getCurrentUser() === null){
-        history.push("/login");
-    }*/
-    let user = AuthService.getUserInfo();
-    console.log(user);
+    const history = useHistory();
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const [firstName,setFirstName] = useState(AuthService.getCurrentUser() == null ? "firstname" : AuthService.getCurrentUser().firstname)
-    const [lastName,setLastName] = useState(AuthService.getCurrentUser() == null ? "lastname" : AuthService.getCurrentUser().lastName)
-    const [pseudo,setPseudo] = useState(AuthService.getCurrentUser() == null ? "pseudo" : AuthService.getCurrentUser().pseudo)
-    const [mdp,setMdp] = useState(AuthService.getCurrentUser() == null ? "password" : AuthService.getCurrentUser().password)
-    const [tel,setTel] = useState("0622901123")
-    const [mail,setMail] = useState(AuthService.getCurrentUser() == null ? "email" : AuthService.getCurrentUser().email)
+    if (AuthService.getCurrentUser() === null) {
+        history.push("/login");
+    }
+
+
+    useEffect(() => {
+        dispatch(getUserByEmail(AuthService.getCurrentUserEmail()));
+    }, [dispatch]);
+
+    const user = useSelector((state) => state.userReducer);
+
+    const [dataUser, setDataUser] = useState();
+
+    const loadUserData = async () => {
+        let userData = await user;
+        setDataUser(userData);
+    }
+    loadUserData().then(()=>console.log(dataUser));
+
+
+/*
+    useEffect(() => {
+        dispatch(getFollowed(dataUser["id"]));
+    }, [dataUser, dispatch]);
+    const followed = useSelector((state) => state.userReducer);
+
+    const [dataFollowed, setDataFollowed] = useState();
+
+    const loadFollowedData = async () => {
+        let followedData = await followed;
+        setDataFollowed(followedData);
+    }
+    loadFollowedData().then(()=>console.log(dataFollowed));
+
+    useEffect(() => {
+        dispatch(getFollowers(dataUser["id"]));
+    }, [dataUser, dispatch]);
+    const followers = useSelector((state) => state.userReducer);
+
+    const [dataFollowers, setDataFollowers] = useState();
+
+    const loadFollowersData = async () => {
+        let followersData = await followers;
+        setDataFollowers(followersData);
+    }
+    loadFollowersData().then(()=>console.log(dataFollowers));*/
+
+
+
+
+    const {register, handleSubmit, watch, formState: {errors}} = useForm();
+    const [firstName, setFirstName] = useState(
+            dataUser === undefined ?
+            "firstname" :
+            dataUser["firstname"]
+)
+    ;
+    const [lastName, setLastName] = useState(
+        dataUser === undefined ?
+            "lastname" :
+            dataUser["lastname"]
+    );
+    const [pseudo, setPseudo] = useState(
+        dataUser === undefined ?
+            "pseudo" :
+            dataUser["pseudo"]
+    );
+    const [mdp, setMdp] = useState(
+        dataUser === undefined ?
+            "password" :
+            dataUser["password"]
+    );
+    const [tel, setTel] = useState("0622901123");
+    const [mail, setMail] = useState(
+        AuthService.getCurrentUser() == null ?
+            "email" :
+            AuthService.getCurrentUserEmail
+    );
+
 
     const onSubmit = data => {
-        console.log(data);
+        console.log(data["firstname"]);
 
-        if(data.firstName !== AuthService.getCurrentUser().firstName){
-            dispatch(updateUserFirstName(/*userId a mettre*//*AuthService.getCurrentUser().id,*/ data));
+        if (data["firstname"] !== dataUser["firstname"]) {
+            dispatch(updateUserFirstName(dataUser["id"], data));
         }
 
-        if(data.lastName !== AuthService.getCurrentUser().lastName){
-            dispatch(updateUserLastName(/*userId a mettre*//*AuthService.getCurrentUser().id,*/ data));
+        if (data["lastname"] !== dataUser["lastname"]) {
+            dispatch(updateUserLastName(dataUser["id"], data));
         }
 
-        if(data.email !== AuthService.getCurrentUser().email){
-            dispatch(updateUserEmail(/*userId a mettre*//*AuthService.getCurrentUser().id,*/ data));
+        if (data.email !== AuthService.getCurrentUser().email) {
+            dispatch(updateUserEmail(dataUser["id"], data));
         }
 
-        if(data.pseudo !== AuthService.getCurrentUser().pseudo){
-            dispatch(updateUserPseudo(/*userId a mettre*//*AuthService.getCurrentUser().id,*/ data));
+        if (data["pseudo"] !==  dataUser["pseudo"]) {
+            dispatch(updateUserPseudo(dataUser["id"], data));
         }
 
-        if(data.password !== AuthService.getCurrentUser().password){
-            dispatch(updateUserPassword(/*userId a mettre*//*AuthService.getCurrentUser().id,*/ data));
+        if (data["password"] !== dataUser["password"]) {
+            dispatch(updateUserPassword(dataUser["id"], data));
         }
 
-        wait(2000).then( r => {
+        wait(2000).then(r => {
             history.push({
-                pathname:"/profil"
+                pathname: "/profil"
             })
         })
     }
@@ -81,17 +156,20 @@ const Profil = () => {
                     <div className="container-profile">
                         <div className="social-profile-input">
                             <div className="title-input">Nom</div>
-                            <input type="text"  {...register("lastName")} className="input-profile" defaultValue={lastName}/>
+                            <input type="text"  {...register("lastName")} className="input-profile"
+                                   defaultValue={lastName}/>
                         </div>
                         <div className="social-profile-input">
                             <div className="title-input">Prenom</div>
-                            <input type="text"  {...register("firstName")} className="input-profile" defaultValue={firstName}/>
+                            <input type="text"  {...register("firstName")} className="input-profile"
+                                   defaultValue={firstName}/>
                         </div>
                     </div>
                     <div className="container-profile">
                         <div className="social-profile-input">
                             <div className="title-input">Pseudo</div>
-                            <input type="text"  {...register("pseudo")} className="input-profile" defaultValue={pseudo}/>
+                            <input type="text"  {...register("pseudo")} className="input-profile"
+                                   defaultValue={pseudo}/>
                         </div>
                         <div className="social-profile-input">
                             <div className="title-input">MDP</div>
