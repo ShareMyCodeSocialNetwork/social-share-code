bfafd60caf901c27ec859920dd18a4c4183355ae
xof <ljehanno@myges.fr>
2022-06-16 23:50:34 +0200

    :ambulance: fix error in route name

--- src/actions/API/follower.action.js
+++ src/actions/API/follower.action.js
@@ -14,7 +14,7 @@ export const DELETE_FOLLOWER = "DELETE_FOLLOWER";
 export const getAll = () => {
     return (dispatch) => {
         return axios
-            .get(`${API_URL}/follower`, { headers: AuthService.authHeader() })
+            .get(`${API_URL}/follow`, { headers: AuthService.authHeader() })
             .then((res) => {
                 dispatch({ type: GET_ALL, payload: res.data });
 
@@ -26,7 +26,7 @@ export const getAll = () => {
 export const getOneFollowerById = (followerId) => {
     return (dispatch) => {
         return axios
-            .get(`${API_URL}/follower/${followerId}`,{ headers:  AuthService.authHeader() })
+            .get(`${API_URL}/follow/${followerId}`,{ headers:  AuthService.authHeader() })
             .then((res) => {
                 dispatch({ type: GET_FOLLOWER_BY_ID, payload: res.data });
             })
@@ -37,7 +37,7 @@ export const getOneFollowerById = (followerId) => {
 export const addFollower = (data) => {
     return (dispatch) => {
         return axios
-            .post(`${API_URL}/follower/create`, data, { headers:  AuthService.authHeader() })
+            .post(`${API_URL}/follow/create`, data, { headers:  AuthService.authHeader() })
             .then(() => {
                 dispatch({ type: ADD_FOLLOWER, payload: data });
             })
@@ -49,7 +49,7 @@ export const addFollower = (data) => {
 export const getFollowers = (followedId) => {
     return (dispatch) => {
         return axios
-            .get(`${API_URL}/follower/followed/${followedId}`,{ headers:  AuthService.authHeader() })
+            .get(`${API_URL}/follow/followed/${followedId}`,{ headers:  AuthService.authHeader() })
             .then((res) => {
                 dispatch({ type: GET_FOLLOWER, payload: res.data });
             })
@@ -60,7 +60,7 @@ export const getFollowers = (followedId) => {
 export const getFollowed = (userId) => {
     return (dispatch) => {
         return axios
-            .get(`${API_URL}/follower/follower/${userId}`,{ headers:  AuthService.authHeader() })
+            .get(`${API_URL}/follow/follower/${userId}`,{ headers:  AuthService.authHeader() })
             .then((res) => {
                 dispatch({ type: GET_FOLLOWED, payload: res.data });
             })
@@ -72,7 +72,7 @@ export const deleteFollower = (followerId) => {
     return (dispatch) => {
         return axios({
             method: "delete",
-            url: `${API_URL}/follower/delete/${followerId}`,
+            url: `${API_URL}/follow/delete/${followerId}`,
             headers:  AuthService.authHeader()
 
         })
