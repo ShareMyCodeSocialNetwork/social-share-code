dec4afd3b616bb91de8268a49d2c74e317552686
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-06-29 22:12:30 +0200

    :rocket: project view and add button remove prject view

--- src/components/pages/ProjectView.js
+++ src/components/pages/ProjectView.js
@@ -1,27 +1,49 @@
 import React, {useState} from 'react';
 import CodeMirror from "@uiw/react-codemirror";
+import {useForm} from "react-hook-form";
+import {deleteCode} from "../../actions/API/code.action";
+import {useDispatch} from "react-redux";
+import {deleteProject} from "../../actions/API/project.action";
 
-const ProjectView = ({name= "",description= "", userPseudo="", projectId = "" }) => {
+const ProjectView = ({name= "",description= "", userPseudo="", projectId = "" , userId = ""}) => {
 
+    const dispatch = useDispatch();
 
+    const remove = useForm();
+
+    const submitRemove = () => {
+        dispatch(deleteProject(projectId));
+        window.location.reload();
+    }
 
     return (
         <div className="component--card-search-code">
+            <a href={"/project/" + projectId}>
             <div className="container-code">
                 <div className="language-code">
                     <div className="language-title">{name}</div>
                 </div>
                 <div className="codemirror">
-                    {description}
+                    <CodeMirror
+                        options={{theme : "default" }}
+                        value={description}
+                        height="100%"
+                        />
                 </div>
             </div>
+            </a>
             <div className="social-code-search">
+                <a href={"/profile/" + userId}>
                 <div className="profile-editor">
                     <img className="profile-img" src="/assets/logo/profil.svg" alt="profile" />
                     <div className="title-name-editor">{userPseudo}</div>
                 </div>
-                <a className="title-name-editor" href={"/project/" + projectId}>Show it</a>
+                </a>
+                <form>
+                    <button onClick={remove.handleSubmit(submitRemove)} className="button-profile" type="submit">Remove</button>
+                </form>
             </div>
+
         </div>
     );
 };
