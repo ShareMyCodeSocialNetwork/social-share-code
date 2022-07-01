8e0ee6b080e17c742fcbc5397747eb26473d6a20
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-07-01 18:28:54 +0200

    :rocket: add create code in group

--- src/pages/ProjectContent.js
+++ src/pages/ProjectContent.js
@@ -5,12 +5,28 @@ import {getOneProjectsById} from "../actions/API/project.action";
 import {getCodeByProject} from "../actions/API/code.action";
 import {isEmpty} from "../components/utils/Utils";
 import MyCodeView from "../components/pages/MyCodeView";
+import NewPen from "../layout/Modals/NewPen";
 
 const ProjectContent = () => {
-    const history = useHistory();
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 838,
+        bgcolor: '#131417',
+        borderRadius:5,
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+    };
+
+    const [openModalPen, setOpenModalPen] = useState(false);
+    const handleCloseModalPen = () => setOpenModalPen(false);
+    const handleOpenModalPen = () => setOpenModalPen(true);
     const { id } = useParams();
     const dispatch = useDispatch();
-
+    const user_id = localStorage.getItem("user_id");
     useEffect(() => {
         dispatch(getOneProjectsById(id));
         dispatch(getCodeByProject(id));
@@ -30,12 +46,8 @@ const ProjectContent = () => {
             //history.push( "/");
             //window.location.reload();
         }
-        console.log(dataProject);
-        console.log("dataProject");
         let codesData = await codes;
         setDataCode(codesData);
-        console.log("dataCode");
-        console.log(dataCode);
     }
     loadProjectData().then();
 
@@ -55,7 +67,11 @@ const ProjectContent = () => {
             {!isEmpty(dataProject) && isEmpty(dataProject.group) && "No group for this project"}
             <br/>
             <br/>
-
+            {
+                !isEmpty(dataProject) && !isEmpty(dataProject.user) &&
+                dataProject.user.id.toString() === user_id.toString() &&
+                <button onClick={() => handleOpenModalPen()}>create code in project</button>
+            }
 
             <div className="view--project">
                 <div className="container-project">
@@ -71,7 +87,9 @@ const ProjectContent = () => {
 
         </div>
         </div>
+            <NewPen style={style} handleCloseModalPen={handleCloseModalPen} openModalPen={openModalPen} project_id={!isEmpty(dataProject) && dataProject.id}></NewPen>
         </div>
+
     );
 
 
