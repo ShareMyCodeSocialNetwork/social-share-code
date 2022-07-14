81feef41f295bf24d130905523cfe9aaac8aa14a
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-07-14 19:25:17 +0200

    :art: searchview connect with api

--- src/pages/SearchView.js
+++ src/pages/SearchView.js
@@ -1,15 +1,8 @@
 import React, {useEffect, useState} from 'react';
-import CardSearchCode from "../components/pages/CardSearchCode";
 import {useParams} from "react-router-dom";
 import {filter_array, isEmpty} from "../components/utils/Utils";
-import {getOneUserById} from "../actions/API/user.action";
-import {getByFollowedAndFollower, getFollowed, getFollowers} from "../actions/API/follower.action";
-import {getProjectByOwner} from "../actions/API/project.action";
-import {getPostByUserId, getPosts} from "../actions/API/post.action";
-import {getCodeByUser} from "../actions/API/code.action";
-import {getGroupsByOwner} from "../actions/API/group.action";
+import {getPosts} from "../actions/API/post.action";
 import {useDispatch, useSelector} from "react-redux";
-import postReducer from "../reducers/API/post.reducer";
 import PostView from "./PostView";
 
 const SearchView = () => {
