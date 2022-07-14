d9ac8481a44525c52c3986fc89234d4270ca943a
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-07-14 19:18:42 +0200

    :art: searchview connect with api

--- src/pages/SearchView.js
+++ src/pages/SearchView.js
@@ -1,27 +1,53 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import CardSearchCode from "../components/pages/CardSearchCode";
 import {useParams} from "react-router-dom";
-import {filter_array} from "../components/utils/Utils";
+import {filter_array, isEmpty} from "../components/utils/Utils";
+import {getOneUserById} from "../actions/API/user.action";
+import {getByFollowedAndFollower, getFollowed, getFollowers} from "../actions/API/follower.action";
+import {getProjectByOwner} from "../actions/API/project.action";
+import {getPostByUserId, getPosts} from "../actions/API/post.action";
+import {getCodeByUser} from "../actions/API/code.action";
+import {getGroupsByOwner} from "../actions/API/group.action";
+import {useDispatch, useSelector} from "react-redux";
+import postReducer from "../reducers/API/post.reducer";
+import PostView from "./PostView";
 
 const SearchView = () => {
+    const dispatch = useDispatch();
 
     const { filters } = useParams();
 
-    const [arrayPostCode, setArrayPostCode] = useState([{"language":"Python","code":"print('hello world')","like":20,"comments":[],"view":1000,"editorName":"David-Henri arnaud"},{"language":"Python","code":"print('hello world')","like":20,"comments":[],"view":1000,"editorName":"David-Henri arnaud"},{"language":"Python","code":"print('hello world')","like":20,"comments":[],"view":1000,"editorName":"David-Henri arnaud"},{"language":"Python","code":"print('hello world')","like":20,"comments":[],"view":1000,"editorName":"David-Henri arnaud"},{"language":"Python","code":"print('hello world')","like":20,"comments":[],"view":1000,"editorName":"David-Henri arnaud"},{"language":"Python","code":"print('hello world')","like":20,"comments":[],"view":1000,"editorName":"David-Henri arnaud"},{"language":"Python","code":"print('hello world')","like":20,"comments":[],"view":1000,"editorName":"David-Henri arnaud"},{"language":"Js","code":"console.log('hello world')","like":20,"comments":[],"view":1000,"editorName":"David-Henri arnaud"},{"language":"Js","code":"console.log('hello world')","like":20,"comments":[],"view":1000,"editorName":"Henri "}])
 
+    useEffect(() => {
+        dispatch(getPosts());
+    }, []);
 
+    const [arrayPost, setArrayPost] = useState([]);
+    const posts = useSelector((state) => state.postReducer);
 
-    const handleAddLike = () => {
-
+    const loadPost = async () => {
+        let dbPosts = await posts;
+        setArrayPost(dbPosts);
+        console.log(arrayPost)
     }
+    loadPost().then()
 
     return (
         <div className="view--project">
             <div className="container-project">
                 {
-                    filter_array(filters,arrayPostCode).map((value, index) =>
+                    !isEmpty(arrayPost) && !isEmpty(filters) &&
+                        filter_array(filters,arrayPost).map((value, index) =>
+                            <div className="post-code" key={index}>
+                                <PostView  postData={value}></PostView>
+                            </div>
+                        )
+                }
+                {
+                    isEmpty(filters) && !isEmpty(arrayPost) &&
+                    arrayPost.map((value, index) =>
                         <div className="post-code" key={index}>
-                            <CardSearchCode language={value.language} nameOfcode={"Python hello world"} code={value.code} like={value.like} comments={value.comments} view={value.view} editorName={value.editorName} handleAddLike={handleAddLike} />
+                            <PostView  postData={value}></PostView>
                         </div>
                     )
                 }
