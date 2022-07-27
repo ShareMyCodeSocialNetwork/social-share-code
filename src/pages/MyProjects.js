54e6243ddaed840971bfc993f510ca0c20a3d716
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-07-27 18:27:36 +0200

    :art: fix project content

--- src/pages/MyProjects.js
+++ src/pages/MyProjects.js
@@ -4,13 +4,10 @@ import {GET_PROJECT_BY_OWNER, getProjectByOwner} from "../actions/API/project.ac
 import {isEmpty} from "../components/utils/Utils";
 import ProjectView from "../components/pages/ProjectView";
 import AuthService from "../components/Auth/AuthService";
-import ReloadUntilData from "../components/utils/ReloadUntilData";
 import axios from "axios";
 import {API_URL} from "../actions/global";
-
 const MyProjects = () => {
     AuthService.isAuth();
-    const dispatch = useDispatch();
     const user_id = localStorage.getItem("user_id");
     const [dataProjects, setDataProjects] = useState([]);
     useEffect(() => {
@@ -21,35 +18,22 @@ const MyProjects = () => {
                 setDataProjects(res.data);
             })
             .catch((err) => console.log(err));
-    }, [dispatch, user_id]);
-
-
-
-   /* const loadProjectData = async () => {
-        let projectsData = await projects;
-        setDataProjects(projectsData);
-        console.log(dataProjects);
-    }
-    loadProjectData().then(r => ReloadUntilData(projects));*/
-
-
+    }, []);
     return (
         <div className="view--project">
             <div className="container-project">
-            {
-                !isEmpty(dataProjects) &&
-                dataProjects.map(
-                    (item, index) => (
-                        <div className="post-code" >
-                            <ProjectView  key={index} userPseudo={item.user.pseudo} name={item.name} description={item.description} projectId={item.id} userId={item.user.id}></ProjectView>
-                        </div>
-                    ))
-            }
+                {
+                    !isEmpty(dataProjects) &&
+                    dataProjects.map(
+                        (item, index) => (
+                            <div key={index} className="post-code" >
+                                <ProjectView  userPseudo={item.user.pseudo} name={item.name} description={item.description} projectId={item.id} userId={item.user.id}></ProjectView>
+                            </div>
+                        ))
+                }
                 {isEmpty(dataProjects) && "No project found ! Create one on top right !"}
-
             </div>
-            </div>
-        );
-
+        </div>
+    );
 }
 export default MyProjects;
--- src/pages/ProjectContent.js
+++ src/pages/ProjectContent.js
@@ -8,7 +8,6 @@ import NewPen from "../layout/Modals/NewPen";
 import AuthService from "../components/Auth/AuthService";
 import axios from "axios";
 import {API_URL} from "../actions/global";
-
 const ProjectContent = () => {
     AuthService.isAuth();
     const style = {
@@ -23,12 +22,10 @@ const ProjectContent = () => {
         boxShadow: 24,
         p: 4,
     };
-
     const [openModalPen, setOpenModalPen] = useState(false);
     const handleCloseModalPen = () => setOpenModalPen(false);
     const handleOpenModalPen = () => setOpenModalPen(true);
     const { id } = useParams();
-    const dispatch = useDispatch();
     const user_id = localStorage.getItem("user_id");
     const [dataProject, setDataProject] = useState();
     useEffect(() => {
@@ -36,82 +33,55 @@ const ProjectContent = () => {
             .get(`${API_URL}/project/full/${id}`,{ headers:  AuthService.authHeader() })
             .then((res) => {
                 setDataProject(res.data)
-            })
-            .catch((err) => console.log(err));
-        //dispatch(getOneProjectsByIdFull(id));
-        //dispatch(getCodeByProject(id));
-    }, [dispatch, id]);
-
-    //const projects = useSelector( state => state.projectReducer);
-    //const codes = useSelector( state => state.codeReducer);
-
-    //const [dataCode, setDataCode] = useState([]);
-
-
-    /*const loadProjectData = async () => {
-        let projectsData = await projects;
-        setDataProject(projectsData);
-        //let codesData = await codes;
-        //setDataCode(codesData);
-    }
-    loadProjectData().then();*/
-
-    /*
-    *
-            {!isEmpty(dataProject) && "project name : " + dataProject.name}
-            <br/>
-            <br/>
-            {!isEmpty(dataProject) && !isEmpty(dataProject.project) && "project description : " + dataProject.project.description}
-            <br/>
-            <br/>
-
-            {!isEmpty(dataProject) && isEmpty(dataProject.group) && "No group for this project"}
-            <br/>
-            <br/>
-    *
-    * */
-
+            }).catch((err) => console.log(err));
+    }, []);
     return (
         <>
-            <NewPen style={style} handleCloseModalPen={handleCloseModalPen} openModalPen={openModalPen} project_id={!isEmpty(dataProject) && dataProject.id}/>
+        <NewPen style={style} handleCloseModalPen={handleCloseModalPen} openModalPen={openModalPen} project_id={!isEmpty(dataProject) && !isEmpty(dataProject.project) && dataProject.project.id}></NewPen>
             <div className="view--project-content">
                 <div className="left-part">
-                    {
-                        isEmpty(dataProject) &&
-                        <div className="information-title">project not found</div>
-                    }
+            {
+                isEmpty(dataProject) && <div className="information-title">project not found</div>
+            }
+            {!isEmpty(dataProject) && !isEmpty(dataProject.project) &&
+                <div className="information-title">project name :  {dataProject.project.name}</div>
+            }
 
-                    {
-                        !isEmpty(dataProject) && !isEmpty(dataProject.project) && !isEmpty(dataProject.project.group) &&
-                        <a className="link-information" href={"/group/" + dataProject.project.group.id}>project group : { dataProject.project.group.name}</a>
-                    }
-                    {
-                        !isEmpty(dataProject) && !isEmpty(dataProject.project) && !isEmpty(dataProject.project.user) && dataProject.project.user.id.toString() === user_id.toString() &&
-                        <a className="link-information" href={"/code/new" + dataProject.project.group.id}>create code in project</a>
-                    }
+            {!isEmpty(dataProject) && !isEmpty(dataProject.project) &&
+                <div className="information-title">project description : {dataProject.project.description}</div>
+                }
+            <br/>
+            <br/>
+            {!isEmpty(dataProject) && !isEmpty(dataProject.project) && !isEmpty(dataProject.project.group) &&
+                <a className="link-information" href={"/group/" + dataProject.project.group.id}>project group : { dataProject.project.group.name}</a>}
+            {!isEmpty(dataProject) && !isEmpty(dataProject.project) && isEmpty(dataProject.project.group) &&
+                <div className="information-title">No group for this project</div>}
+            <br/>
+            <br/>
+            {
+                !isEmpty(dataProject) && !isEmpty(dataProject.project) && !isEmpty(dataProject.project.owner) &&
+                dataProject.project.owner.id.toString() === user_id.toString() &&
+                <a className="link-information" href={"/code/new" + dataProject.project.id}>create code in project</a>
+            }
                 </div>
                 <div className="right-part">
-                    <div className="container-project">
-                        {
-                            !isEmpty(dataProject) && !isEmpty(dataProject.codesInProject) &&
-                            dataProject.codesInProject.map((item, index) => (
-                                <div key={index} className="post-code">
-                                    <MyCodeView language={item.language.name} code={item.nameCode} userPseudo={item.user.pseudo} codeId={item.id} userId={item.user.id}></MyCodeView>
-                                </div>
-                            ))
-                        }
-                        {
-                            !isEmpty(dataProject) && isEmpty(dataProject.codesInProject) &&
-                            <div className="information-title">"No codes for this project"</div>
-                        }
-                    </div>
+            <div className="view--project">
+                <div className="container-project">
+                    {
+                        !isEmpty(dataProject) && !isEmpty(dataProject.codesInProject) &&
+                        !isEmpty(dataProject.codesInProject) &&
+                        dataProject.codesInProject.map((item, index) => (
+                            <div key={index} className="post-code">
+                                <MyCodeView language={item.language.name} code={item.nameCode} userPseudo={item.user.pseudo} codeId={item.id} userId={item.user.id}></MyCodeView>
+                            </div>))
+                    }
+                    {!isEmpty(dataProject) && isEmpty(dataProject.codesInProject) && <div className="information-title">No codes for this project</div>}
                 </div>
             </div>
+            </div>
+
+        </div>
         </>
-
     );
-
-
 };
-
 export default ProjectContent;
