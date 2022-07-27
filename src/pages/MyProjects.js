8111cd741ff51e00d527fcd603402f8e9b00d310
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-07-27 15:28:58 +0200

    :ambulance: fix my project

--- src/pages/MyProjects.js
+++ src/pages/MyProjects.js
@@ -1,41 +1,47 @@
 import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import { getProjectByOwner} from "../actions/API/project.action";
+import {GET_PROJECT_BY_OWNER, getProjectByOwner} from "../actions/API/project.action";
 import {isEmpty} from "../components/utils/Utils";
 import ProjectView from "../components/pages/ProjectView";
 import AuthService from "../components/Auth/AuthService";
 import ReloadUntilData from "../components/utils/ReloadUntilData";
+import axios from "axios";
+import {API_URL} from "../actions/global";
 
 const MyProjects = () => {
     AuthService.isAuth();
     const dispatch = useDispatch();
     const user_id = localStorage.getItem("user_id");
-
+    const [dataProjects, setDataProjects] = useState([]);
     useEffect(() => {
-        dispatch(getProjectByOwner(user_id));
+        axios
+            .get(`${API_URL}/project/user/${user_id}`,{ headers:  AuthService.authHeader() })
+            .then((res) => {
+                console.log(res.data);
+                setDataProjects(res.data);
+            })
+            .catch((err) => console.log(err));
     }, [dispatch, user_id]);
 
-    const projects = useSelector( state => state.projectReducer);
-    const [dataProjects, setDataProjects] = useState([]);
 
-    const loadProjectData = async () => {
+
+   /* const loadProjectData = async () => {
         let projectsData = await projects;
         setDataProjects(projectsData);
         console.log(dataProjects);
     }
-    loadProjectData().then(r => ReloadUntilData(projects));
+    loadProjectData().then(r => ReloadUntilData(projects));*/
 
 
     return (
         <div className="view--project">
             <div className="container-project">
-
             {
                 !isEmpty(dataProjects) &&
                 dataProjects.map(
                     (item, index) => (
-                        <div className="post-code" >
-                        <ProjectView  key={index} userPseudo={item.user.pseudo} name={item.name} description={item.description} projectId={item.id} userId={item.user.id}></ProjectView>
+                        <div key={index} className="post-code" >
+                        <ProjectView  userPseudo={item.user.pseudo} name={item.name} description={item.description} projectId={item.id} userId={item.user.id}></ProjectView>
                         </div>
                     ))
             }
